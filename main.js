6ef2c6e49f0adf906c76525673874a1995b3c4c7
Ankit <ankit.verma@orvito.com>
2015-06-16 14:58:53 +0530

    first few lines of node

---
+++ main.js
@@ -0,0 +1,10 @@
+var http = require("http");
+
+http.createServer(function(request,response){
+	response.writeHead(200,{'Content-Type':'text/plain'});
+	response.end('hello world');
+
+
+}).listen(8081);
+
+console.log('Server is running at 8081');
