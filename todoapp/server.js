a9c9a88224a208e8a84a4770571b04189b22018b
Ankit <ankit.verma@orvito.com>
2015-07-02 16:26:35 +0530

    todo app ready

--- todoapp/server.js
+++ todoapp/server.js
@@ -1,80 +1,27 @@
-//server.js
+// set up ======================================================================
+var express  = require('express');
+var app      = express(); 								// create our app w/ express
+var mongoose = require('mongoose'); 					// mongoose for mongodb
+var port  	 = process.env.PORT || 8080; 				// set the port
+var database = require('./config/database'); 			// load the database config
 
-//setup ================================
+var morgan = require('morgan'); 		// log requests to the console (express4)
+var bodyParser = require('body-parser'); 	// pull information from HTML POST (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 
-var express = require('express');
-var app = express();
-var mongoose = require('mongoose');
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
-var methodOverRide = require('method-override');
+// configuration ===============================================================
+mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
 
-mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');
+app.use(express.static(__dirname + '/public')); 				// set the static files location /public/img will be /img for users
+app.use(morgan('dev')); 										// log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'})); 			// parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); 									// parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
 
-app.use(express.static(__dirname+'/public'));
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({'extended':'true'}));
-app.use(bodyParser.json());
-app.use(bodyParser.json({type :'application/vnd.api+json'}));
+// routes ======================================================================
+require('./app/routes.js')(app);
 
-var Todo = mongoose.model('Todo',{
-	text: String
-});
-
-
-//routes ===========================================================
-
-	//api-------------------------------------------------------
-	//get all todos
-	api.get('api/todos',function(req,res){
-		
-		Todo.find(function(err,todos){
-			if(err)
-		  	   res.send(err);
-			res.json(todos);
-		});
-	});
-
-	api.post('/api/todos',function(req,res){
-		Todo.create({
-		     text:req.body.text,
-		     done:false;
-		},function(err,todo){
-		  	if(err)
-			   res.send(arr);
-			Todo.find(function(){
-				if(err)
-				   res.send(err)
-				res.json(todos);
-			});
-
-		  });	
-	});
-	app.delete('/api/todos/:todo_id',function(req,res){
-		Todo.remove({
-		   _id:req.params.todo_id
-		},function(err,todo){
-                        if(err)
-                           res.send(arr);
-                        Todo.find(function(){
-                                if(err)
-                                   res.send(err)
-                                res.json(todos);
-                        });
-
-                  });
-        });
-
-
-app.listen(4444);
-console.log("App Listening on port 4444");
-
-
-
-
-
-
-
-
-
-
+// listen (start app with node server.js) ======================================
+app.listen(port);
+console.log("App listening on port " + port);
